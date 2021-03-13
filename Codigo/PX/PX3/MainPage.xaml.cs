2075ef002267b36edd1ee95bacdb5c245dc2e8fc
Steff10740 <36547867+Steff10740@users.noreply.github.com>
2021-03-13 10:35:51 -0400

    Add files via upload

---
+++ Codigo/PX/PX3/MainPage.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace PX3
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        async void Login_Clicked(object sender, EventArgs e)
+        { 
+
+            if (string.IsNullOrEmpty(bEmail.Text))
+            {
+                await DisplayAlert("Advertencia", "Campo Email y/o Password no pueden estar vacios", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", (bEmail.Text), "Ok");
+            }
+            if (string.IsNullOrEmpty(Pw.Text))
+            {
+                await DisplayAlert("Advertencia", "Campo Email y/o Password no pueden estar vacios", "OK");
+            }
+            else
+            {
+                await DisplayAlert("Bienvenido", (bEmail.Text), "Ok");
+            }
+
+        }
+
+        private void Register_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Register());
+        }
+    }
+}
